5f140a24885077bc509c6a1eefd9929904a448c7
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-05-22 00:44:09 +0400

    add client & bind connection

---
+++ greet/greet_client/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"gRPC/greet/greetpb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Yoo, Shady")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("created client: %f", c)
+}
