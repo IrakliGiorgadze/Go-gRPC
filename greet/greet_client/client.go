4a7afc5b85c2ef634c9d1610c9b70fafa1fe989c
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-05-22 18:49:42 +0400

    streaming implementation in server side

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"gRPC/greet/greetpb"
@@ -12,20 +13,21 @@ import (
 
 func main() {
 	fmt.Println("client is running...")
-
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
-
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	doUnary(c)
+
+	//doUnary(c)
+	doServerStreaming(c)
 
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("starting unary RPC...")
 	req := &greetpb.GreetRequest{
 		Greeting: &greetpb.Greeting{
 			FirstName: "Shady",
@@ -37,4 +39,29 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("error calling RPC: %v", err)
 	}
 	log.Printf("response calling RPC: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("starting server streaming RPC...")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Shady",
+			LastName:  "Claus",
+		},
+	}
+
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error calling server streaming: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error reading stream: %v", err)
+		}
+		log.Printf("response from GreetManyTimes: %v", msg.GetResult())
+	}
 }
